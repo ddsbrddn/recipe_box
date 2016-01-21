7297826b5b2c6251fe3fddc6262e6908d3558f13
Dodi Sabaruddin <ddsbrddn@gmail.com>
2016-01-21 09:10:42 +0700

    Add structure and model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,26 @@
 class Recipe < ActiveRecord::Base
+  has_many :ingredients
+  has_many :directions
+  accepts_nested_attributes_for :ingredients,
+                                reject_if:
+                                proc { |attributes| attributes['name'].blank? },
+                                allow_destroy: true
+  accepts_nested_attributes_for :directions,
+                                reject_if:
+                                proc { |attributes| attributes['step'].blank? },
+                                allow_destroy: true
+
+  validates :title, :description, :picture, presence: true
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+
+  private
+
+    # Validates the size of an uploaded pictures.
+    def picture_size
+      if picture.size > 3.megabytes
+        errors.add(:picture, "should be less than 3MB")
+      end
+    end
 end
